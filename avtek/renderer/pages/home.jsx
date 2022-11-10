6f7049d7d043596325e9e4074ebfd95eeb6505d3
Šimen Ravnik <ravnik.simen@gmail.com>
2022-11-10 13:39:26 +0100

    Add initial project structure

---
+++ avtek/renderer/pages/home.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import Head from 'next/head';
+import Link from 'next/link';
+
+function Home() {
+  return (
+    <React.Fragment>
+      <Head>
+        <title>Avtek</title>
+      </Head>
+
+      <div class="container mx-auto px-2">
+
+        <h1 className="font-medium leading-tight text-5xl mt-0 mb-2">Avtek</h1>
+
+        <div className="card shadow-xl">
+          <div className="card-body">
+            <h2 className="card-title">Personal information</h2>
+
+            <div>
+              <label for="first_name" class="block mb-2 text-sm font-medium">First name</label>
+              <input type="text" id="first_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5" placeholder="John" required />
+            </div>
+
+          </div>
+        </div>
+      </div>
+
+
+      <div className='mt-1 w-full flex-wrap flex justify-center'>
+        <Link href='/next'>
+          <a className='btn-blue'>Go to next page</a>
+        </Link>
+      </div>
+
+      <div className="btm-nav">
+        <button>
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" /></svg>
+          <span className="btm-nav-label">Home</span>
+        </button>
+        <button className="active">
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+          <span className="btm-nav-label">Warnings</span>
+        </button>
+        <button>
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" /></svg>
+          <span className="btm-nav-label">Statics</span>
+        </button>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default Home;
---
+++ avtek/renderer/tailwind.config.js
@@ -0,0 +1,21 @@
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  content: [
+    './renderer/pages/**/*.{js,ts,jsx,tsx}',
+    './renderer/components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    colors: {
+      // use colors only specified
+      white: colors.white,
+      gray: colors.gray,
+      blue: colors.blue,
+    },
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"],
+  },
+};

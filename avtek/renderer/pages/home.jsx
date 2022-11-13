01eda43fd27f8f924552766dda5bcecab8b9f05c
Šimen Ravnik <ravnik.simen@gmail.com>
2022-11-13 12:16:21 +0100

    Add other car brands to the project

--- avtek/renderer/pages/home.jsx
+++ avtek/renderer/pages/home.jsx
@@ -6,8 +6,29 @@ import CarModelCard from '../elements/CarModelCard';
 
 function Home() {
 
-  const [activeCarBrandId, setActiveCarBrandId] = React.useState("");
-  const [activeCarModelId, setActiveCarModelId] = React.useState("");
+  const [activeCarBrandId, setActiveCarBrandId] = React.useState("1");
+  const [activeCarModelId, setActiveCarModelId] = React.useState("1");
+
+  const cars = {
+    "1": [
+      { id: "1", brand: "Mercedes", model: "C180", src: "/images/logos/mercedes.png" },
+      { id: "2", brand: "Audi", model: "A5", src: "/images/logos/audi.png" },
+      { id: "3", brand: "Toyota", model: "Avensis", src: "/images/logos/toyota.png" },
+      { id: "4", brand: "Honda", model: "Accord", src: "/images/logos/honda.png" },
+      { id: "5", brand: "Skoda", model: "Octavia", src: "/images/logos/skoda.png" }
+    ],
+    "2": [
+      { id: "1", brand: "Lamborgini", model: "Aventador", src: "/images/logos/lamborgini.png" },
+      { id: "2", brand: "Ferrari", model: "488", src: "/images/logos/ferrari.png" },
+      { id: "3", brand: "Porsche", model: "911", src: "/images/logos/porsche.png" },
+    ],
+    "3": [
+      { id: "1", brand: "Jeep", model: "Wrangler", src: "/images/logos/jeep.png" },
+      { id: "2", brand: "Land Rover", model: "Defender", src: "/images/logos/land-rover.png" },
+      { id: "3", brand: "Toyota", model: "4runner", src: "/images/logos/toyota.png" },
+      { id: "4", brand: "Suzuki", model: "Jimny", src: "/images/logos/suzuki.png" },
+    ],
+  }
 
   return (
     <React.Fragment>
@@ -37,13 +58,7 @@ function Home() {
         </div>
 
         <div className="grid grid-cols-6 gap-10">
-          {[
-            { id: "1", brand: "Mercedes", model: "C180", src: "/images/logos/mercedes.png" },
-            { id: "2", brand: "Audi", model: "A5", src: "/images/logos/audi.png" },
-            { id: "3", brand: "Toyota", model: "Avensis", src: "/images/logos/toyota.png" },
-            { id: "4", brand: "Honda", model: "Accord", src: "/images/logos/honda.png" },
-            { id: "5", brand: "Skoda", model: "Octavia", src: "/images/logos/skoda.png" }
-          ].map(card => (
+          {cars[activeCarBrandId].map(card => (
             <CarModelCard
               key={card.id}
               id={card.id}

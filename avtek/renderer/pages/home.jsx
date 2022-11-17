8182dcac7b607e28e3c2e052f2dc2d429e9fc01c
Šimen Ravnik <ravnik.simen@gmail.com>
2022-11-17 23:10:18 +0100

    Add tabIndex to cards for picking vehicle

--- avtek/renderer/pages/home.jsx
+++ avtek/renderer/pages/home.jsx
@@ -20,7 +20,7 @@ function Home(props) {
             <ul>
               <li>
                 <Link href='/home'>
-                  <a>
+                  <a tabIndex={1} >
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="w-4 h-4 mr-2 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"></path></svg>
                     Domov
                   </a>
@@ -28,7 +28,7 @@ function Home(props) {
               </li>
               <li>
                 <Link href='/second'>
-                  <a>
+                  <a tabIndex={2}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="w-4 h-4 mr-2 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"></path></svg>
                     Podatki o vozilu
                   </a>
@@ -41,7 +41,7 @@ function Home(props) {
                     Osebni podatki
                   </a>
                   : <Link href='/third'>
-                    <a>
+                    <a tabIndex={3}>
                       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="w-4 h-4 mr-2 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"></path></svg>
                       Osebni podatki
                     </a>
@@ -54,7 +54,7 @@ function Home(props) {
                     Plačilo
                   </a>
                   : <Link href='/forth'>
-                    <a>
+                    <a tabIndex={4}>
                       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="w-4 h-4 mr-2 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"></path></svg>
                       Plačilo
                     </a>
@@ -66,7 +66,7 @@ function Home(props) {
 
         <div className="grid grid-cols-3 gap-8">
           {props.brands.map(card => (
-            <div key={card.id} onClick={() => { props.setActiveCarBrandId(card.id); props.setActiveCarModelId("0") }} className={`card mb-5 hover:cursor-pointer ${props.activeCarBrandId === card.id ? "card border shadow-md" : ""}`}>
+            <div tabIndex={parseInt(card.id) + 5} key={card.id} onClick={() => { props.setActiveCarBrandId(card.id); props.setActiveCarModelId("0") }} onKeyPress={() => props.setActiveCarBrandId(card.id)} className={`card mb-5 hover:cursor-pointer ${props.activeCarBrandId === card.id ? "card border shadow-md" : ""}`}>
               <div className='my-2 w-full flex flex-col justify-center'>
                 <img className={`object-contain ${props.activeCarBrandId === card.id ? "scale-100" : "scale-95"} hover:scale-100 ease-in duration-100`} src={card.src} />
                 <label className="block mb-2 text-xl justify-center text-center font-medium">{card.title}</label>
@@ -77,7 +77,7 @@ function Home(props) {
 
         <div className="grid grid-cols-8 gap-10">
           {props.cars[props.activeCarBrandId].map((card) => (
-            <div key={card.id} onClick={() => props.setActiveCarModelId(card.id)} className={`card mb-5 hover:cursor-pointer ${props.activeCarModelId === card.id ? "card border shadow-md" : ""}`}>
+            <div tabIndex={parseInt(card.id) + 8} key={card.id} onClick={() => props.setActiveCarModelId(card.id)} onKeyPress={() => props.setActiveCarModelId(card.id)} className={`card mb-5 hover:cursor-pointer ${props.activeCarModelId === card.id ? "card border shadow-md" : ""}`}>
               <div className='my-2 flex flex-col justify-center'>
                 <div>
                   <img className={`object-contain p-1 ${props.activeCarModelId === card.id ? "scale-100" : "scale-95"} hover:scale-100 ease-in duration-100`} src={card.src} />
